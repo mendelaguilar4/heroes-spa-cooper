afb09473e4cd883991245becd11307822de1c454
Mendel <mendelaguilar1999123@gmail.com>
2024-01-31 14:51:51 -0600

    Primer Commit

---
+++ src/heroes/pages/index.js
@@ -0,0 +1,6 @@
+
+
+export * from './DCPage';
+export * from './HeroPage';
+export * from './MarvelPage';
+export * from './SearchPage';
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,40 @@
+import { Route, Routes } from "react-router-dom"
+
+import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
+
+import { HeroesRoutes } from "../heroes"
+import { LoginPage } from "../auth/"
+
+
+
+
+export const AppRouter = () => {
+  return (
+    <>
+        <Routes>
+           
+            {/* <Route path="login" element={<LoginPage/>} /> */}
+
+            <Route path="login/*" element={
+              <PublicRoute>
+                {/* <LoginPage/> */}
+                <Routes>
+                  <Route path="/*" element={<LoginPage/>} />
+                </Routes>
+
+              </PublicRoute>
+            } />
+
+            <Route path="/*" element={
+              <PrivateRoute>
+                <HeroesRoutes/>
+              </PrivateRoute>
+            } />
+
+            {/* <Route path="/*" element={<HeroesRoutes/>} /> */}
+
+        </Routes>
+    </>
+  )
+}
