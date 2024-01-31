3ad5f30bbf04086e00f8da70c0ce47cbdce7a10e
Mendel <mendelaguilar1999123@gmail.com>
2024-01-31 15:55:53 -0600

    Ajuste en directorio v2

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -23,7 +23,7 @@ export const HeroPage = () => {
     <div className="row mt-5 animate__animated animate__fadeInLeft">
       <div className="col-4">
         <img
-          src={`/heroes/${id}.jpg`}
+          src={`docs/heroes/${id}.jpg`}
           alt={hero.superhero}
           className="img-thumbnail"
         />
