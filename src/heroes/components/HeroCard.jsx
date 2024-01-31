4d680d731f6f370820b71c45f3ac6eaf119f63a7
Mendel <mendelaguilar1999123@gmail.com>
2024-01-31 15:40:27 -0600

    Cambio de rutas en las imágenes - v4

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -22,7 +22,7 @@ export const HeroCard = ({
     characters
 }) => {
 
-    const heroImageUrl = `/heroes/${ id }.jpg`;
+    const heroImageUrl = `/public/heroes/${ id }.jpg`;
 
     // const charactersByHero = (<p>{characters}</p>);
 
