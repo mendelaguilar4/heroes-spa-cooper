c13ab339ff8a0e6d17e386c8d558212e2e76da22
Mendel <mendelaguilar1999123@gmail.com>
2024-02-01 08:19:13 -0600

    Ajustes en rutas de imágenes

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -22,7 +22,7 @@ export const HeroCard = ({
     characters
 }) => {
 
-    const heroImageUrl = `/docs/heroes/${ id }.jpg`;
+    const heroImageUrl = `/docs/assets/${ id }.jpg`;
 
     // const charactersByHero = (<p>{characters}</p>);
 
