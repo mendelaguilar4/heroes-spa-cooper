88ea7ed7525d3184eaf2f7137551f4b7c825e963
Mendel <mendelaguilar1999123@gmail.com>
2024-02-01 08:45:39 -0600

    Ajustes en rutas de imágenes en GitHubPages v2

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -22,7 +22,7 @@ export const HeroCard = ({
     characters
 }) => {
 
-    const heroImageUrl = `/heroes/${ id }.jpg`;
+    const heroImageUrl = `../../heroes/${ id }.jpg`;
 
     // const charactersByHero = (<p>{characters}</p>);
 
